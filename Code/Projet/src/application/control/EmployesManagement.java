d7ccbddec45d338d3c3bd83defbaf8ec7ca37044
IUTB <noreply@univ-tlse2.fr>
2022-06-08 14:34:42 +0200

    haha la blague

--- Code/Projet/src/application/control/EmployesManagement.java
+++ Code/Projet/src/application/control/EmployesManagement.java
@@ -1,130 +1,31 @@
 package application.control;
 
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.ResourceBundle;
 
 import application.DailyBankApp;
 import application.DailyBankState;
 import application.tools.EditionMode;
 import application.tools.StageManagement;
+import application.view.ClientsManagementController;
 import application.view.EmployesManagementController;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.SelectionMode;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.data.Client;
 import model.data.Employe;
+import model.orm.AccessClient;
 import model.orm.AccessEmploye;
 import model.orm.exception.ApplicationException;
 import model.orm.exception.DatabaseConnexionException;
 
 public class EmployesManagement {
-	
-	private EmployesManagement em;
-	private ObservableList<Employe> ole;
+
 	private Stage primaryStage;
 	private DailyBankState dbs;
 	private EmployesManagementController emc;
-	
-	
-	@FXML
-	private ListView<Employe> lvEmployes;
-	@FXML
-	private TextField txtNum;
-	@FXML
-	private TextField txtNom;
-	@FXML
-	private TextField txtPrenom;
-	@FXML
-	private Button btnDesactEmploye;
-	@FXML
-	private Button btnModifEmploye;
-	@FXML
-	private Button btnComptesEmploye;
-	
-	public void initContext(Stage _primaryStage, EmployesManagement _em, DailyBankState _dbstate) {
-		this.em = _em;
-		this.primaryStage = _primaryStage;
-		this.dbs = _dbstate;
-		this.configure();
-		this.doRechercher();
-	}
-	
-	private void configure() {
-		this.primaryStage.setOnCloseRequest(e -> this.closeWindow(e));
 
-		this.ole = FXCollections.observableArrayList();
-		this.lvEmployes.setItems(this.ole);
-		this.lvEmployes.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-		this.lvEmployes.getFocusModel().focus(-1);
-		this.lvEmployes.getSelectionModel().selectedItemProperty().addListener(e -> this.validateComponentState());
-		this.validateComponentState();
-	}
-
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-	}
-	
-	@FXML
-	private void doCancel() {
-		this.primaryStage.close();
-	}
-	
-	@FXML
-	private void doRechercher() {
-		int numCompte;
-		try {
-			String nc = this.txtNum.getText();
-			if (nc.equals("")) {
-				numCompte = -1;
-			} else {
-				numCompte = Integer.parseInt(nc);
-				if (numCompte < 0) {
-					this.txtNum.setText("");
-					numCompte = -1;
-				}
-			}
-		} catch (NumberFormatException nfe) {
-			this.txtNum.setText("");
-			numCompte = -1;
-		}
-
-		String debutNom = this.txtNom.getText();
-		String debutPrenom = this.txtPrenom.getText();
-
-		if (numCompte != -1) {
-			this.txtNom.setText("");
-			this.txtPrenom.setText("");
-		} else {
-			if (debutNom.equals("") && !debutPrenom.equals("")) {
-				this.txtPrenom.setText("");
-			}
-		}
-
-		// Recherche des clients en BD. cf. AccessClient > getClients(.)
-		// numCompte != -1 => recherche sur numCompte
-		// numCompte != -1 et debutNom non vide => recherche nom/prenom
-		// numCompte != -1 et debutNom vide => recherche tous les clients
-		ArrayList<Client> listeCli;
-		listeCli = this.cm.getlisteComptes(numCompte, debutNom, debutPrenom);
-
-		this.olc.clear();
-		for (Client cli : listeCli) {
-			this.olc.add(cli);
-		}
-
-		this.validateComponentState();
-	}
-	
 	/** Constructeur de la classe qui permet de paramétrer la fenêtre
 	 * @param _parentStage la scène parente
 	 * @param _dbstate la session de l'utilisateur
@@ -132,7 +33,7 @@ public class EmployesManagement {
 	public EmployesManagement(Stage _parentStage, DailyBankState _dbstate) {
 		this.dbs = _dbstate;
 		try {
-			FXMLLoader loader = new FXMLLoader(EmployesManagementController.class.getResource("employesmanagement.fxml"));
+			FXMLLoader loader = new FXMLLoader(ClientsManagementController.class.getResource("clientsmanagement.fxml"));
 			BorderPane root = loader.load();
 
 			Scene scene = new Scene(root, root.getPrefWidth()+50, root.getPrefHeight()+10);
@@ -143,7 +44,7 @@ public class EmployesManagement {
 			this.primaryStage.initOwner(_parentStage);
 			StageManagement.manageCenteringStage(_parentStage, this.primaryStage);
 			this.primaryStage.setScene(scene);
-			this.primaryStage.setTitle("Gestion des employés");
+			this.primaryStage.setTitle("Gestion des clients");
 			this.primaryStage.setResizable(false);
 
 			this.emc = loader.getController();
@@ -166,15 +67,15 @@ public class EmployesManagement {
 	 * @param c	le client sélectionné
 	 * @return le client avec les informations modifiées
 	 */
-	public Employe modifierEmploye(Employe e) {
+	public Employe modifierEmploye(Employe em) {
 		EmployeEditorPane cep = new EmployeEditorPane(this.primaryStage, this.dbs);
-		Employe result = cep.doEmployeEditorDialog(e, EditionMode.MODIFICATION);
+		Employe result = cep.doEmployeEditorDialog(em, EditionMode.MODIFICATION);
 		if (result != null) {
 			try {
-				AccessEmploye ae = new AccessEmploye();
-				ae.updateEmploye(result);
-			} catch (DatabaseConnexionException exc) {
-				ExceptionDialog ed = new ExceptionDialog(this.primaryStage, this.dbs, exc);
+				AccessEmploye ac = new AccessEmploye();
+				ac.updateEmploye(result);
+			} catch (DatabaseConnexionException e) {
+				ExceptionDialog ed = new ExceptionDialog(this.primaryStage, this.dbs, e);
 				ed.doExceptionDialog();
 				result = null;
 				this.primaryStage.close();
@@ -196,9 +97,9 @@ public class EmployesManagement {
 		employe = cep.doEmployeEditorDialog(null, EditionMode.CREATION);
 		if (employe != null) {
 			try {
-				AccessEmploye ae = new AccessEmploye();
+				AccessEmploye ac = new AccessEmploye();
 
-				ae.insertEmploye(employe);
+				ac.insertEmploye(employe);
 			} catch (DatabaseConnexionException e) {
 				ExceptionDialog ed = new ExceptionDialog(this.primaryStage, this.dbs, e);
 				ed.doExceptionDialog();
@@ -211,5 +112,35 @@ public class EmployesManagement {
 			}
 		}
 		return employe;
+	}
+
+	/** Permet de rechercher la liste des comptes clients en fonction des paramètres
+	 * @param _numCompte numéro de compte recherché
+	 * @param _debutNom	nom du client recherché
+	 * @param _debutPrenom	prenom du client recherché
+	 * @return une liste de compte client selon les paramètres de recherche
+	 */
+	public ArrayList<Employe> getlisteComptes(int _numCompte, String _debutNom, String _debutPrenom) {
+		ArrayList<Employe> listeEmp = new ArrayList<>();
+		try {
+			// Recherche des clients en BD. cf. AccessClient > getClients(.)
+			// numCompte != -1 => recherche sur numCompte
+			// numCompte != -1 et debutNom non vide => recherche nom/prenom
+			// numCompte != -1 et debutNom vide => recherche tous les clients
+
+			AccessEmploye ac = new AccessEmploye();
+			listeEmp = ac.getEmployes(this.dbs.getEmpAct().idAg, _numCompte, _debutNom, _debutPrenom);
+
+		} catch (DatabaseConnexionException e) {
+			ExceptionDialog ed = new ExceptionDialog(this.primaryStage, this.dbs, e);
+			ed.doExceptionDialog();
+			this.primaryStage.close();
+			listeEmp = new ArrayList<>();
+		} catch (ApplicationException ae) {
+			ExceptionDialog ed = new ExceptionDialog(this.primaryStage, this.dbs, ae);
+			ed.doExceptionDialog();
+			listeEmp = new ArrayList<>();
+		}
+		return listeEmp;
 	}
 }
