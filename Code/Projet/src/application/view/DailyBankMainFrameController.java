396b7a6a0ccbbfe0af152268bc948b5b08c23da6
Kizo02 <58359645+Sean0203@users.noreply.github.com>
2022-06-12 19:40:27 +0200

    Revert "Merge branch 'devThomas'"
    This reverts commit b900e218f92adb1a89f45586e92565fd3903f8e8, reversing
    changes made to 5b8bd8d2a11a09e95db674108d8ce317394e8ec7.

--- Code/Projet/src/application/view/DailyBankMainFrameController.java
+++ Code/Projet/src/application/view/DailyBankMainFrameController.java
@@ -83,8 +83,6 @@ public class DailyBankMainFrameController implements Initializable {
 	@FXML
 	private Label lblEmpPrenom;
 	@FXML
-	private Label lblEmpID;
-	@FXML
 	private MenuItem mitemClient;
 	@FXML
 	private MenuItem mitemEmploye;
@@ -159,7 +157,6 @@ public class DailyBankMainFrameController implements Initializable {
 			this.lblAdrAg.setText(a.adressePostaleAg);
 			this.lblEmpNom.setText(e.nom);
 			this.lblEmpPrenom.setText(e.prenom);
-			this.lblEmpID.setText(Integer.toString(e.idEmploye));
 			if (this.dbs.isChefDAgence()) {
 				this.mitemEmploye.setDisable(false);
 			} else {
@@ -175,7 +172,6 @@ public class DailyBankMainFrameController implements Initializable {
 			this.lblAdrAg.setText("");
 			this.lblEmpNom.setText("");
 			this.lblEmpPrenom.setText("");
-			this.lblEmpID.setText("");
 
 			this.mitemClient.setDisable(true);
 			this.mitemEmploye.setDisable(true);
@@ -199,7 +195,8 @@ public class DailyBankMainFrameController implements Initializable {
 	 */
 	@FXML
 	private void doEmployeOption() {
-		this.dbmf.gestionEmployes();
+		AlertUtilities.showAlert(this.primaryStage, "Gestion des Employé", "En cours de développement",
+				"Livraison prévue\nEn juin 2022", AlertType.INFORMATION);
 	}
 
 	/**
--- Code/Projet/src/model/data/Employe.java
+++ Code/Projet/src/model/data/Employe.java
@@ -30,7 +30,9 @@ public class Employe {
 
 	@Override
 	public String toString() {
-		return "["+ this.idEmploye + "] " + this.nom + " " + this.prenom + " (" + this.droitsAccess + ")";
+		return "Employe [idEmploye=" + this.idEmploye + ", nom=" + this.nom + ", prenom=" + this.prenom
+				+ ", droitsAccess=" + this.droitsAccess + ", login=" + this.login + ", motPasse=" + this.motPasse
+				+ ", idAg=" + this.idAg + "]";
 	}
 
 }
